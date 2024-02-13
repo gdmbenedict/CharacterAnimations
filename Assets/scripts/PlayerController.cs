80886de0e2e83c97aaf24993561806511c01f50c
Matthieu Benedict <97464794+Twitchton@users.noreply.github.com>
2024-02-13 18:05:21 -0400

    Sprite Atlas & Running
    Re-generated the sprite atlas to fix tearing issues. Added the running feature.

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Vector2 movement;
-    public float movementSpeed = 2f;
+    [SerializeField] private float movementSpeed;
+
+    [SerializeField] private Animator animator;
+    [SerializeField] private float runSpeed =7.5f;
+    [SerializeField] private float walkSpeed = 5f;
+
+    [SerializeField]  private bool running;
+    [SerializeField] private bool idle;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        movementSpeed = walkSpeed;
     }
 
     private void Awake()
@@ -22,15 +30,41 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-
-        movement.Normalize();
+        HandleInputs();
     }
 
     
     private void FixedUpdate()
     {
+        HandleMovement();
+    }
+
+    private void HandleAnimations()
+    {
+        animator.SetFloat("MoveX", movement.x);
+        animator.SetFloat("MoveY", movement.y);
+    }
+
+    private void HandleMovement()
+    {
+        movement.Normalize();
         rb.MovePosition((Vector2)transform.position + (movement * movementSpeed * Time.fixedDeltaTime));
+    }
+
+    private void HandleInputs()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            running = true;
+            movementSpeed = runSpeed;
+        }
+        else
+        {
+            running = false;
+            movementSpeed = walkSpeed;
+        }
     }
 }
